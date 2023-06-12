b684cefbf6a2503480b119e38508285cc1532bf6
Akin Olusanya <aktope@gmail.com>
2023-06-12 14:21:11 +0100

    Initial Commit

---
+++ ProjectNewsApp/ProjectNewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectNewsAppApp.swift
+//  ProjectNewsApp
+//
+//  Created by Akin Olusanya on 12/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct ProjectNewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
